58f7b799befcbb8248b29030f10275b109835d4a
Teodora Mitrevska <tm5683@student.uni-lj.si>
2023-01-03 23:01:12 +0100

    okay

--- src/components/Additional.jsx
+++ src/components/Additional.jsx
@@ -15,89 +15,17 @@ function Additional(){
       <div className="header"><img src={logo}></img></div>
         <div className='main-container'>
             <div className="container">   
+                
                 <div className="scale">
                     <ScaleCleanliness/>
                 </div>
-            </div>
-
-        {/* <div className="container"> 
-            <h2 className="title">Location </h2>
-                <div className="scale">
-                    <span class="material-icons md-48" onClick={()=>{valueEmoji=6; setShow(!show); setHideLightbox(false)}}>
-                        mood_bad
-                    </span>
-                    <span class="material-icons md-48" onClick={()=>{valueEmoji=7;setShow(!show); setHideLightbox(false)}}>
-                        sentiment_dissatisfied
-                    </span>
-                    <span class="material-icons md-48"  onClick={()=>{valueEmoji=8; setShow(!show); setHideLightbox(false)}}>
-                            sentiment_neutral
-                    </span>
-                    <span class="material-icons md-48" onClick={()=>{valueEmoji=9;setShow(!show); setHideLightbox(false)}}>
-                        sentiment_satisfied_alt
-                    </span>
-                    <span class="material-icons md-48" onClick={()=>{valueEmoji=10; console.log(valueEmoji); setShow(!show);  setHideLightbox(true)}}>
-                        mood
-                    </span>
-                </div>
-                <div className ='tags'>
-                    {
-                        show && (valueEmoji===10) ?
-                        <div> <button className='tag'>Private parking</button>
-                        <button className='tag'>Wheelchair accesible</button>
-                        <button className='tag'>Garage</button>
-                        <button className='tag'>City center</button>
-                        </div>:null
-                    
-                    }
                 
-
-                    {
-                        show && (valueEmoji===9) ?
-                        <div><button className='tag'>Street parking nearby</button>
-                        <button className='tag'>Supermarket nearby</button>
-                        <button className='tag'>Central location</button>
-                        <button className='tag'>Bakery nearby</button>
-                        <button className='tag'>Spacious</button>
-                        <button className='tag'>Renovated</button>
-                        <button className='tag'>Quiet</button></div>:null
-                    
-                    }     
-
-                    {
-                        show && (valueEmoji===8) ?
-                        <div><button className='tag'>
-                            Public transportation available
-                        </button>
-                        <button className='tag'>Suburbs</button>
-                        <button className='tag'>Private parking</button>
-                        <button className='tag'>Access to highway</button>
-                        <button className='tag'>Supermarket nearby</button></div>:null
-                    
-                    } 
-
-                    {
-                        show && (valueEmoji===7) ?
-                        <div><button className='tag'>Limited public transportation available</button>
-                        <button className='tag'>No restaurants or cafes nearby</button>
-                        <button className='tag'>No supermarket nearby</button></div>:null
-                    
-                    }
-
-                    {
-                    
-                        show && (valueEmoji===8) ?
-                        <div><button className='tag'>No parking nearby</button>
-                        <button className='tag'>Bad road conditions</button><button className='tag'>No public transportation available</button>
-                        <button className='tag'>Unclear instructions to accomodation</button></div>:null
-                    
-                    } 
-                
-                </div>
             </div>
-                */}        
+            
+     
         </div>
-
-        
+        <a href="./AdditionalLocation" target="_self" rel="noreferrer">
+        <button className='comment-button'>Next</button></a>
     </div>
     
     
@@ -105,21 +33,5 @@ function Additional(){
 
 
 
-/*function ShowMore(){
 
-    const [show,setShow]=useState(false);
-    const [hideLightbox, setHideLightbox] = useState(true); 
-    {setShow(!show); 
-    setHideLightbox(false)}
-    
-        
-        show ?
-        <div><button className='tag'>Dirty sheets</button>
-        <button className='tag'>Bad service</button>
-        <button className='tag'>Cold</button></div>:null
-        
-        
-
-
-}*/
 export default Additional;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import OverallRating from './components/OverallRating';
 import Additional from './components/Additional';
-
+import AdditionalLocation from "./components/AdditionalLocation"
+import End from "./components/End"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
@@ -17,6 +18,8 @@ root.render(
       <Route path="/Dislikes" element={<Dislikes />} />
       <Route path="/OverallRating" element={<OverallRating />} />
       <Route path="/Additional" element={<Additional/>} />
+      <Route path="/AdditionalLocation" element={<AdditionalLocation/>} />
+      <Route path="/End" element={<End/>} />
       
     </Routes>
   </Router>
@@ -28,3 +31,6 @@ export { default as Dislikes } from "./components/Dislikes";
 export { default as Home } from "./components/Home";
 export { default as OverallRating } from "./components/OverallRating";
 export { default as Additional } from "./components/Additional";
+export { default as AdditionalLocation } from "./components/AdditionalLocation";
+export { default as End } from "./components/End";
+
